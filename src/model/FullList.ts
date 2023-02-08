8a33267846747d95574482fb8a08e54be93613c4
Michael Pacleb <michael.pacleb@gmail.com>
2023-02-08 15:59:16 -0500

    Created model/class FullList.

---
+++ src/model/FullList.ts
@@ -0,0 +1,63 @@
+import ListItem from "./ListItem";
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(id: string): void;
+}
+
+class FullList implements List {
+  static instance: FullList = new FullList();
+
+  private constructor(private _list: ListItem[] = []) {}
+
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myTodoList");
+
+    // Type guard. It should be "string" because in the save method, we stringify our list.
+    if (typeof storedList !== "string") return;
+
+    // We cant use the ListItem[] type because the prop keys has prepended underscore.
+    // So we just created a similar type on the fly
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList);
+
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      );
+
+      FullList.instance.addItem(newListItem);
+    });
+  }
+
+  save(): void {
+    localStorage.setItem("myTodoList", JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id);
+    this.save();
+  }
+}
+
+export default FullList;
